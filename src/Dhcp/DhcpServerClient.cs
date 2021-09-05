dafce106e9641aa73a0f3a491cfc59bc5c9023ef
PolarBears <polarbearscn@outlook.com>
2021-09-06 04:29:45 +0800

    Revert "Revert "Fix #1 enumerated clients""
    This reverts commit 6e173a1f8a34f48ed45f347694a4e0d49dca5578.

--- src/Dhcp/DhcpServerClient.cs
+++ src/Dhcp/DhcpServerClient.cs
@@ -218,6 +218,7 @@ namespace Dhcp
                         yield break; // Last results
 
                     Api.FreePointer(clientsPtr);
+                    clientsPtr = IntPtr.Zero;
                     result = Api.DhcpEnumSubnetClients(ServerIpAddress: server.Address,
                                                        SubnetAddress: (DHCP_IP_ADDRESS)0,
                                                        ResumeHandle: ref resultHandle,
@@ -275,6 +276,7 @@ namespace Dhcp
                         yield break; // Last results
 
                     Api.FreePointer(clientsPtr);
+                    clientsPtr = IntPtr.Zero;
                     result = Api.DhcpEnumSubnetClientsVQ(ServerIpAddress: server.Address,
                                                          SubnetAddress: (DHCP_IP_ADDRESS)0,
                                                          ResumeHandle: ref resultHandle,
@@ -335,6 +337,7 @@ namespace Dhcp
                         yield break; // Last results
 
                     Api.FreePointer(clientsPtr);
+                    clientsPtr = IntPtr.Zero;
                     result = Api.DhcpEnumSubnetClients(ServerIpAddress: scope.Server.Address,
                                                        SubnetAddress: scope.Address.ToNativeAsNetwork(),
                                                        ResumeHandle: ref resultHandle,
@@ -385,6 +388,7 @@ namespace Dhcp
                         yield break; // Last results
 
                     Api.FreePointer(clientsPtr);
+                    clientsPtr = IntPtr.Zero;
                     result = Api.DhcpEnumSubnetClientsVQ(ServerIpAddress: scope.Server.Address,
                                                          SubnetAddress: scope.Address.ToNativeAsNetwork(),
                                                          ResumeHandle: ref resultHandle,
